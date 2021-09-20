623332b928d3c6afe45f5be3b725287f12619d45
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-09-20 04:37:33 -0700

    add gallery

--- app/src/main/java/educa/movel/com/rv/RvGallery.java
+++ app/src/main/java/educa/movel/com/rv/RvGallery.java
@@ -2,6 +2,8 @@ package educa.movel.com.rv;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +14,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.squareup.picasso.Picasso;
 
+import java.io.Serializable;
 import java.util.List;
 
 import educa.movel.com.R;
 import educa.movel.com.model.Image;
+import educa.movel.com.ui.ImagesFullScreen;
 
 
 public class RvGallery extends RecyclerView.Adapter<RvGallery.MyViewHoder> {
@@ -43,6 +47,18 @@ public class RvGallery extends RecyclerView.Adapter<RvGallery.MyViewHoder> {
         if (!img.isEmpty()) {
             Picasso.get().load(mData.get(position).getImg()).into(holder.img);
         }
+
+        holder.img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext , ImagesFullScreen.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("images" , (Serializable) mData);
+                intent.putExtras(bundle);
+                intent.putExtra("position" , position);
+                mContext.startActivity(intent);
+            }
+        });
 
     }
 
