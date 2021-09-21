4e46778d054e4c5f6a2b71a48c95e99007cceb67
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-09-21 03:09:00 -0700

    create game

--- app/src/main/java/educa/movel/com/ui/game/GameActivity.java
+++ app/src/main/java/educa/movel/com/ui/game/GameActivity.java
@@ -23,6 +23,8 @@ public class GameActivity extends AppCompatActivity {
     private TextView tv_question;
     private List<Question> questionList = new ArrayList<>();
     private int currentClickedBtn = 0;
+    private int currentQuestion = 0;
+    private int playerPoints = 0;
 
 
     @Override
@@ -36,6 +38,7 @@ public class GameActivity extends AppCompatActivity {
         window.setStatusBarColor(Color.BLACK);
 
         initUI();
+        setQuestionUI();
 
         btn_option_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,23 +49,115 @@ public class GameActivity extends AppCompatActivity {
         });
 
 
+        btn_option_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentClickedBtn = 2;
+                checkAnswer();
+            }
+        });
+
+
+        btn_option_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentClickedBtn = 3;
+                checkAnswer();
+            }
+        });
+
+
+        btn_option_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentClickedBtn = 4;
+                checkAnswer();
+            }
+        });
+
+
     }
 
     private void checkAnswer() {
+        String correctAnswer = questionList
+                .get(currentQuestion)
+                .getCorrectAnswer()
+                .trim();
 
-        switch (currentClickedBtn) {
+        if (isCorrect(correctAnswer, getAnswer())) {
+            playerPoints++;
+            nextGame();
+        } else  {
+            btn_option_4.setBackgroundResource(R.drawable.btn_bg_erro);
+            nextGame();
+        }
+
+    }
+
+    private void nextGame() {
+        showTheCorrectAnswer();
+        updateUI();
+    }
+
+    private void updateUI() {
+        btn_option_1.setBackgroundResource(R.drawable.btn_bg_1);
+        btn_option_2.setBackgroundResource(R.drawable.btn_bg_1);
+        btn_option_3.setBackgroundResource(R.drawable.btn_bg_1);
+        btn_option_4.setBackgroundResource(R.drawable.btn_bg_1);
+
+
+    }
+
+    private void setQuestionUI() {
+        tv_option_1.setText(questionList.get(currentQuestion).getAnswer1());
+        tv_option_2.setText(questionList.get(currentQuestion).getAnswer2());
+        tv_option_3.setText(questionList.get(currentQuestion).getAnswer3());
+        tv_option_4.setText(questionList.get(currentQuestion).getAnswer4());
+
+        tv_question.setText(questionList.get(currentQuestion).getQuestion());
+    }
+
+    private void showTheCorrectAnswer() {
+        if (tv_option_1.getText().toString().trim().equalsIgnoreCase(questionList.get(currentQuestion)
+                .getCorrectAnswer().trim())) {
+            btn_option_1.setBackgroundResource(R.drawable.btn_bg_certo);
+        }
+
+        if (tv_option_2.getText().toString().trim().equalsIgnoreCase(questionList.get(currentQuestion)
+                .getCorrectAnswer().trim())) {
+            btn_option_2.setBackgroundResource(R.drawable.btn_bg_certo);
+        }
+
+        if (tv_option_3.getText().toString().trim().equalsIgnoreCase(questionList.get(currentQuestion)
+                .getCorrectAnswer().trim())) {
+            btn_option_3.setBackgroundResource(R.drawable.btn_bg_certo);
+        }
+
+        if (tv_option_4.getText().toString().trim().equalsIgnoreCase(questionList.get(currentQuestion)
+                .getCorrectAnswer().trim())) {
+            btn_option_4.setBackgroundResource(R.drawable.btn_bg_certo);
+        }
+    }
+
+    private String getAnswer() {
+        String answer = "";
+        switch (currentQuestion) {
             case 1:
-                if (isCorrect(, tv_option_1.getText().toString().trim()))
+                answer = tv_option_1.getText().toString().trim().toLowerCase();
                 break;
             case 2:
+                answer = tv_option_2.getText().toString().trim().toLowerCase();
                 break;
             case 3:
+                answer = tv_option_3.getText().toString().trim().toLowerCase();
                 break;
             case 4:
+                answer = tv_option_4.getText().toString().trim().toLowerCase();
                 break;
             default:
                 break;
         }
+        return answer;
     }
 
     private boolean isCorrect(String correctAnswer, String userAnswer) {
@@ -97,5 +192,7 @@ public class GameActivity extends AppCompatActivity {
         tv_option_2 = (TextView) findViewById(R.id.tv_option_2);
         tv_option_3 = (TextView) findViewById(R.id.tv_option_3);
         tv_option_4 = (TextView) findViewById(R.id.tv_option_4);
+
+        tv_question = (TextView) findViewById(R.id.tv_question);
     }
 }
