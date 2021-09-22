64a82fde898c8ac1cb2a87b87d5b51c8c44de2c7
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-09-22 01:42:29 -0700

    add icon

---
+++ app/src/main/java/educa/movel/com/model/User.java
@@ -0,0 +1,112 @@
+package educa.movel.com.model;
+
+public class User {
+    String  category, contact, date_of_birth,
+            email, genre, imageUrl, name, password,
+            residence, school, userId;
+
+    public User(String category, String contact, String date_of_birth, String email, String genre, String imageUrl, String name, String password, String residence, String school, String userId) {
+        this.category = category;
+        this.contact = contact;
+        this.date_of_birth = date_of_birth;
+        this.email = email;
+        this.genre = genre;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.password = password;
+        this.residence = residence;
+        this.school = school;
+        this.userId = userId;
+    }
+
+    public User() {
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getResidence() {
+        return residence;
+    }
+
+    public void setResidence(String residence) {
+        this.residence = residence;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
--- app/src/main/java/educa/movel/com/ui/login/CreateAccountActivity.java
+++ app/src/main/java/educa/movel/com/ui/login/CreateAccountActivity.java
@@ -2,15 +2,82 @@ package educa.movel.com.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import educa.movel.com.R;
+import educa.movel.com.model.User;
+import educa.movel.com.utils.CheckField;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    private Button btn_create;
+    private EditText ed_email, ed_name, ed_number, ed_password;
+    private TextView have_account;
+    private ProgressBar progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        initUI();
+
+        btn_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkFields();
+            }
+        });
+    }
+
+    private void checkFields() {
+        CheckField field = new CheckField();
+        String name = ed_name.getText().toString().trim();
+        String email = ed_email.getText().toString().trim();
+        String number = ed_number.getText().toString().trim();
+        String password = ed_password.getText().toString().trim();
+
+        if (field.isEmail(name)) {
+            ed_name.requestFocus();
+            ed_name.setHintTextColor(Color.RED);
+        } else if (field.isEmail(email)) {
+            ed_email.requestFocus();
+            ed_email.setHintTextColor(Color.RED);
+        } else if (field.isEmpty(number)) {
+            ed_number.requestFocus();
+            ed_number.setHintTextColor(Color.RED);
+        } else if (field.isEmpty(password)) {
+            ed_password.requestFocus();
+            ed_password.setHintTextColor(Color.RED);
+        } else {
+            setProgressState(true);
+
+            User user = new User("", number, "" , email ,
+                    "", "", name, password, "", "", "");
+        }
+    }
+
+    private void setProgressState(boolean inProgress) {
+        if (inProgress) {
+            progress.setVisibility(View.VISIBLE);
+            btn_create.setVisibility(View.GONE);
+        } else {
+            progress.setVisibility(View.GONE);
+            btn_create.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void initUI() {
+        btn_create = (Button) findViewById(R.id.btn_enter);
+        ed_email = findViewById(R.id.ed_email);
+        ed_name = findViewById(R.id.ed_name);
+        ed_number = findViewById(R.id.ed_number);
+        ed_password = findViewById(R.id.ed_password);
+        progress = findViewById(R.id.progress);
     }
 }
