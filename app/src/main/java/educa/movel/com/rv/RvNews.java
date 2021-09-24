81e926c83c30b16fbf36003882a1af70a17e309a
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-09-24 01:59:37 -0700

    add news

--- app/src/main/java/educa/movel/com/rv/RvNews.java
+++ app/src/main/java/educa/movel/com/rv/RvNews.java
@@ -2,9 +2,11 @@ package educa.movel.com.rv;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +17,7 @@ import java.util.List;
 
 import educa.movel.com.R;
 import educa.movel.com.model.News;
+import educa.movel.com.ui.InfoFullActivity;
 
 
 public class RvNews extends RecyclerView.Adapter<RvNews.MyViewHoder> {
@@ -37,9 +40,13 @@ public class RvNews extends RecyclerView.Adapter<RvNews.MyViewHoder> {
 
     @Override
     public void onBindViewHolder(@NonNull final MyViewHoder holder, final int position) {
-
-
-
+        holder.ll_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, InfoFullActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -49,10 +56,11 @@ public class RvNews extends RecyclerView.Adapter<RvNews.MyViewHoder> {
 
     public static class MyViewHoder extends RecyclerView.ViewHolder{
 
+        LinearLayout ll_info;
 
         public MyViewHoder(@NonNull View itemView) {
             super(itemView);
-
+            ll_info = itemView.findViewById(R.id.ll_info);
         }
     }
 }
---
+++ app/src/main/java/educa/movel/com/ui/InfoFullActivity.java
@@ -0,0 +1,28 @@
+package educa.movel.com.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import educa.movel.com.R;
+
+public class InfoFullActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_full);
+        initUI();
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    private void initUI() {
+        toolbar = findViewById(R.id.toolbar);
+    }
+}
