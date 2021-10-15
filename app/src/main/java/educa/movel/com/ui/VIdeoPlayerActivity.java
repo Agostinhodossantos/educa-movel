64c1b6a8b239960ccfc5bc2f0e13614c51f8cdc8
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-10-15 01:14:49 -0700

    create video player

---
+++ app/src/main/java/educa/movel/com/ui/VIdeoPlayerActivity.java
@@ -0,0 +1,43 @@
+package educa.movel.com.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import educa.movel.com.R;
+
+public class VIdeoPlayerActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+    private String url;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+        videoView = findViewById(R.id.videoView);
+
+        videoView.setVideoURI(Uri.parse(url));
+        videoView.setMediaController(new MediaController(this));
+        videoView.requestFocus();
+        videoView.start();
+    }
+
+
+    private void getIntentValues() {
+        Intent intent = getIntent();
+        if(getIntent()!=null && getIntent().getExtras()!=null){
+            Bundle bundle = getIntent().getExtras();
+            if(!bundle.getString("url").equals(null)) {
+                url = intent.getExtras().getString("url");
+            }else {
+                Toast.makeText(this, "Ocoreu uma falha", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+}
